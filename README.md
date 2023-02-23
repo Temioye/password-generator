13e1cdbbf810aa664b05dbc8aab6826721c4569b
temio <temioyelade8@gmail.com>
2023-02-23 13:39:49 +0000

    created var for arrays and password length

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # password-generator
+
+## Description
+
+An application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code.
+
+## User Story
+
+## Screenshot
+
+## link to deployed page
--- assets/script.js
+++ assets/script.js
@@ -1,136 +1,113 @@
-REQUIREMENTS
-// Generate a password when the button is clicked
-// Present a series of prompts for password criteria
-//Length of password
-//At least 10 characters but no more than 64.
-//Character types
-//Lowercase
-//Uppercase
-//Numeric
-//Special characters ($@%&*, etc)
-//Code should validate for each input and at least one character type should be selected
-//Once prompts are answered then the password should be generated and displayed in an alert or written to the page
-
-
-
-
-
 
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
-
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
+// Get references for #generate and #password element
+var generateBtn = document.querySelector("#generate");
 
 // Function to generate password with user input
 function generatePassword() {
+  // Empty arrays must be local variables in order to reset. 
+  var finalPassword = "";
+  var userSelection = [];
 
-}
+  // Password Length
+  var passLength = window.prompt(
+    "How long do you want your password to be? (Must be between 10 and 64 characters)"
+  );
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+
+
+
+
+
